de57f7292c8a4d60063b33788cef3222ee9e01f1
mateus-oliveira <matews5522@gmail.com>
2020-06-13 20:07:08 -0300

    Animação do dia dos namorados

---
+++ README.md
@@ -0,0 +1,17 @@
+# Animação para o dia dos namorados
+
+Animação feita com módulo turtle de Python, mostrando uma imagem do casal no centro da tela e corações em volta que piscam.
+
+### Conceitos abordados:
+
+* Orientação a objetos;
+* Álgebra linear;
+* Importação de bibliotecas (turtle e random).
+
+### Para baixar, execute:
+
+```bash
+git clone https://github.com/mateus-oliveira/dia-dos-namorados.git
+cd dia-dos-namorados/
+python dia_dos_namorados.py
+```
---
+++ dia_dos_namorados.py
@@ -0,0 +1,62 @@
+from turtle import Pen, Screen
+from random import randint
+
+
+tela = Screen()
+
+class Coracao:
+    def __init__(self):
+        self._desenho = Pen()
+        self._desenho.hideturtle()
+        self._desenho.speed('fastest')
+    
+    def desenhar_coracao(self, cor:str):
+        x = randint(-700, 700)
+        y = randint(-400, 400)
+        self._desenho.color(cor)
+        self._desenho.up()
+        self._desenho.goto(x, y)
+        self._desenho.down()
+        self._desenho.begin_fill()
+        self._desenho.setheading(45)
+        self._desenho.forward(50)
+        self._desenho.circle(25, 180)
+        self._desenho.setheading(135)
+        self._desenho.circle(25, 180)
+        self._desenho.forward(50)
+        self._desenho.end_fill()
+    
+    def esconder_coracao(self):
+        self._desenho.clear()
+
+
+class Imagem:
+    def __init__(self):
+        self._desenho = Pen()
+        self._desenho.shape('img.gif')
+
+
+def main():
+    try: 
+        tela.title('Casa comigo?')
+        tela.setup(700, 700)
+        tela.register_shape('img.gif')
+
+        imagem = Imagem()
+        
+        coracao = Coracao()
+
+        cores = ('red', 'blue', 'green', 'gold', 'pink', 'purple')
+        
+        while True:
+            tela.onkey(tela.bye, 'space')
+            tela.listen()
+            for i in range(6):
+                coracao.desenhar_coracao(cores[i])
+            
+            coracao.esconder_coracao()
+
+    except: pass
+
+if __name__ == '__main__':
+    main()
